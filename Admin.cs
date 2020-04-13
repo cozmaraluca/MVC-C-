dde8060bdb60332d1e2fbddcda394dbc35d815ac
cozmaraluca <38536846+cozmaraluca@users.noreply.github.com>
2020-04-13 14:41:58 +0300

    Add files via upload

---
+++ Admin.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace AdminGSS.Models
+{
+    public class Admin
+    {
+        public class Clienti
+        {
+            public int Id { get; set; }
+            [Required(ErrorMessage = "Introduceti numele")]
+            [RegularExpression(@"^[a-zA-Z]+$", ErrorMessage = "Folositi doar litere")]
+            public string Nume { get; set; }
+            [Required(ErrorMessage = "Introduceti parola")]
+            [RegularExpression(@"^(?=.*\d)(?=.*[A-Z]).{4,8}$",ErrorMessage ="Format parola gresit")]
+            public string Parola { get; set; }
+            
+        }
+    }
+}
