fcac3700bc9217fd92057b2c1b8e1be41224d685
cozmaraluca <38536846+cozmaraluca@users.noreply.github.com>
2020-04-13 14:35:21 +0300

    Add files via upload

---
+++ Clienti.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace GrandSmartphoneStore.Models
+{
+    public class Clienti
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage ="Introduceti numele")]
+        [RegularExpression(@"^[a-zA-Z]+$", ErrorMessage = "Folositi doar litere")]
+        public string Nume { get; set; }
+        [Required(ErrorMessage = "Introduceti marca")]
+        public string Marca { get; set; }
+        [Required(ErrorMessage = "Introduceti modelul")]
+        [RegularExpression("[^0-11]", ErrorMessage = "Modelul nu exista")]
+        public int Model { get; set; }
+    }
+}
---
+++ Database.cs
@@ -0,0 +1,24 @@
+﻿using GrandSmartphoneStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GrandSmartphoneStore.Repo
+{
+
+    public static class ClientiRepo
+    {
+        private static List<Clienti> _lista = new List<Clienti>();
+
+        public static void Adaugare(Clienti client)
+        {
+            _lista.Add(client);
+        }
+        public static IEnumerable<Clienti> GetClienti()
+        {
+            return _lista;
+        }
+
+    }
+}
